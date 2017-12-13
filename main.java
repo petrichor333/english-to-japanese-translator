b47dce23e2f120a692d12eb4456e5f8091d0fcfd
Matthew <appmaker4fun@outlook.com>
2017-12-13 14:37:13 -0800

    Added Content
    This will translate 1 word at a time.

--- main.java
+++ main.java
@@ -1,7 +1,21 @@
+import java.util.Scanner;
+
 public class main {
 	public static void main(String[] args) {
 		String[] EnglishWords = {"book", "chair", "clock", "desk", "eraser", "pencil", "room", "school", "telephone"};
 		String[] JapaneseWords = {"ほん", "いす", "とけい", "つくえ", "けしごむ", "えんぴつ", "へや", "がっこう", "でんわ"};
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a word to translate: ");
+		String english = sc.next();
+		for (int i = 0; i < EnglishWords.length; i++) {
+			if(english.equals(EnglishWords[i])) {
+				System.out.println(JapaneseWords[i]);
+			}
+		}
+		
+		
+		
 		
 	}
 
