f161ccfebec46e153917bf4232e2298b619c39ea
Matthew <appmaker4fun@outlook.com>
2017-12-13 14:07:32 -0800

    Main English to Japanese translator

---
+++ main.java
@@ -0,0 +1,8 @@
+public class main {
+	public static void main(String[] args) {
+		String[] EnglishWords = {"book", "chair", "clock", "desk", "eraser", "pencil", "room", "school", "telephone"};
+		String[] JapaneseWords = {"ほん", "いす", "とけい", "つくえ", "けしゴム", "えんぴつ", "へや", "がっこう", "でんわ"};
+		
+	}
+
+}
