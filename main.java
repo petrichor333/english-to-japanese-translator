6f9b9401e7cb3616f3b5808488c4af855f58d98a
petrichor333 <32719530+petrichor333@users.noreply.github.com>
2017-12-13 14:11:14 -0800

    Update main.java

--- main.java
+++ main.java
@@ -1,7 +1,7 @@
 public class main {
 	public static void main(String[] args) {
 		String[] EnglishWords = {"book", "chair", "clock", "desk", "eraser", "pencil", "room", "school", "telephone"};
-		String[] JapaneseWords = {"ほん", "いす", "とけい", "つくえ", "けしゴム", "えんぴつ", "へや", "がっこう", "でんわ"};
+		String[] JapaneseWords = {"ほん", "いす", "とけい", "つくえ", "けしごむ", "えんぴつ", "へや", "がっこう", "でんわ"};
 		
 	}
 
